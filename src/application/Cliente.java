aeb6924ab6f9b091d0425e24145a9033248d5967
Richard Roncancio <batusai513@msn.com>
2015-05-27 02:22:10 -0500

    wop

--- src/application/Cliente.java
+++ src/application/Cliente.java
@@ -31,15 +31,21 @@ public class Cliente {
     
     public void crearConexion(){
         try {
-            this.conectarAlServidor();
-            int i = 0;
-            while(running){
+            do{
+                this.conectarAlServidor();
                 this.obtenerFlujos();
                 this.correrAplicacion();
-            }
+            }while(running);
+
         } catch (IOException ex) {
             ex.printStackTrace();
             Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            try {
+                this.cerrarConexiones();
+            } catch (IOException ex) {
+                Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
     
@@ -54,6 +60,12 @@ public class Cliente {
         entrada = new DataInputStream(socket.getInputStream());
     }
     
+    public void cerrarConexiones() throws IOException{
+        entrada.close();
+        salida.close();
+        socket.close();
+    }
+    
     public static void main(String[] args) {
         Cliente c = new Cliente();
         c.crearConexion();
@@ -66,7 +78,7 @@ public class Cliente {
         System.out.println("Escriba su clave");
         String clave = sc.nextLine();
         
-        salida.writeUTF("session/create:email=" + email + "&password=" + clave);
+        this.enviarMensaje("session/create:email=" + email + "&password=" + clave);
         
         String respuesta = entrada.readUTF();
         String[] arrayRespuesta = respuesta.split(":");
@@ -74,15 +86,51 @@ public class Cliente {
         
         if (estado.equals("SUCCESS")) {
             String datos = arrayRespuesta[1];
-            
-            this.clienteActual = new model.Cliente();
-            this.clienteActual.toObject(datos);
-            
-            System.out.println("Bienvenido, " + this.clienteActual.getNombre());
-            salida.writeUTF("EXIT");
-            running = false;
+            this.renderWelcome(datos);
+            this.renderMemu();
         } else {
             System.out.println("Error de autenticacion");
         }
+    }
+    
+    private void enviarMensaje(String mensaje) throws IOException{
+        salida.writeUTF(mensaje);
+        salida.flush();
+    }
+    
+    public void renderWelcome(String datos){
+        this.clienteActual = new model.Cliente();
+        this.clienteActual.toObject(datos);
+
+        System.out.println("Bienvenido, " + this.clienteActual.getNombre());
+    }
+    
+    public void renderMemu() throws IOException{
+
+        boolean exit=false;
+        do{
+            System.out.println("1. Ver saldo");
+            System.out.println("2. Pagar servicios");
+            System.out.println("3. Salir");
+
+            int opcion = sc.nextInt();
+
+            switch(opcion) {
+                case 1:
+                    this.enviarMensaje("balance/show:client_id=" + clienteActual.getClienteId());
+                    break;
+                case 2:
+                    System.out.println("No tiene servicios a pagar");
+                    break;
+                case 3:
+                    System.out.println("Good Bye!");
+                    this.enviarMensaje("EXIT");
+                    exit = true;
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Opcion Invalida");
+            }
+        }while(!exit);
     }
 }
